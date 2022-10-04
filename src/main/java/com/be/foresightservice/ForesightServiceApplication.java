d34fe521a6c0a4f5166d9f53a00b6be6b561b05d
unknown <markjayson.gonzaga1990@gmail.com>
2022-10-04 12:59:20 +0800

    initial files

---
+++ src/main/java/com/be/foresightservice/ForesightServiceApplication.java
@@ -0,0 +1,13 @@
+package com.be.foresightservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForesightServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ForesightServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/be/foresightservice/ForesightServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.be.foresightservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForesightServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
